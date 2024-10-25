659d27aac8c71e66fef9e8020e81ebbe20fc91c5
Vivek Kumar <90414513+VivekKumar2380@users.noreply.github.com>
2024-10-25 14:55:52 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Codebank",
+    lastName: "vivek",
+    email: "vivek.kumar@codebank.in",
+  };
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || "Guest"}
+            subtext="Access and manage your account and transactions with ease"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={29876.56}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1250.56 }, {currentBalance: 3560.72 }]}
+      />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
