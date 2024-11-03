f7296dbb6ea4bcd22ba68f34306a293b3e2f8d6d
Vivek Kumar <90414513+VivekKumar2380@users.noreply.github.com>
2024-11-03 14:48:07 +0530

    Implement Plaid Link and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -16,15 +17,16 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import { signIn, signUp ,getLoggedInUser} from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
+import { Input } from "./ui/input";
 
 const AuthForm = ({ type }: { type: string }) => {
-    const router= useRouter();
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   // const loggedInUser = await getLoggedInUser();
@@ -45,21 +47,33 @@ const AuthForm = ({ type }: { type: string }) => {
     try {
       //  Sign up with Appwrite & create plaid token
 
-      if(type === "sign-up") {
-        const newUser=await signUp(data)
+      if (type === "sign-up") {
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        };
+        const newUser = await signUp(userData);
         setUser(newUser);
       }
 
-      if(type === "sign-in") {
+      if (type === "sign-in") {
         const response = await signIn({
-            email: data.email,
-            password: data.password,
-        })
-        console.log(response)
-        if(response) router.push("/");
+          email: data.email,
+          password: data.password,
+        });
+
+        if (response) router.push("/");
       }
     } catch (error) {
-        console.log(error);
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
@@ -90,7 +104,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/*PlaidLink */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <>
           <Form {...form}>
@@ -117,7 +133,7 @@ const AuthForm = ({ type }: { type: string }) => {
                     label="Address"
                     placeholder="Enter your specific address"
                   />
-                   <CustomInput
+                  <CustomInput
                     control={form.control}
                     name="city"
                     label="City"
